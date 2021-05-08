46b4d43081e574489ea9c7f1a7397f96e5c7b61e
ilanataha <81433217+ilanataha@users.noreply.github.com>
2021-05-08 14:43:12 +0300

    Update hw4_skeleton_2021b.py

--- hw4_skeleton_2021b.py
+++ hw4_skeleton_2021b.py
@@ -36,27 +36,25 @@ def min_mult(L):
     return min_mult
 
 # b
-def best_mat_mult_time_fast(L):
-    if len(L) ==3:
-        return L[0]*L[1]*L[2]
-    (a,b) = first_mult(L)
-    L.pop(b[0]+1)
-    return min_mult_fast(L, a) + b[1]
-
-def first_mult(L):
-    mu = {}
-    for i in range(len(L)-2):
-        p = L[i]*L[i+1]*L[i+2]
-        mu[i]=p
-    min_mult = min(mu.items(), key=lambda x: x[1])
-    mu.pop(min_mult[0])
-    return mu, min_mult
-
 def min_mult_fast(L,mu):
     min_mult = min(mu.items(), key=lambda x: x[1])
     mu.pop(min_mult[0])
     L.pop(min_mult[0])
+    min_mult_fast(L, mu)
     return min_mult, L, mu
+
+def best_mat_mult_time_fast(L):
+    if len(L) ==3:
+        return L[0]*L[1]*L[2]
+    mu = {}
+    for i in range(len(L) - 2):
+        p = L[i] * L[i + 1] * L[i + 2]
+        mu[i] = p
+    min_mu = min(mu.items(), key=lambda x: x[1])
+    mu.pop(min_mu[0])
+    L.pop(min_mu[0]+1)
+    print(L)
+    return min_mult_fast(L, mu)[0] + min_mu[1]
 
 print(best_mat_mult_time_fast([100,10,100,10])
 
